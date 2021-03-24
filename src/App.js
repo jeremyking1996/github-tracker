11f1d2ce08f7630f7d06177e913ccb43d1cffc3b
Jeremy King <jeremyking@Jeremys-Air.fios-router.home>
2021-03-24 16:16:21 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Form, Card, Image, Icon } from 'semantic-ui-react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [userName, setUsername] = useState('');
+  const [follower, setFollowers] = useState('');
+  const [following, setFollowing] = useState('');
+  const [repos, setRepos] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/example')
+    .then(res => res.json())
+    .then(data => {
+      setData(data)
+    })
+  }, []);
+
+    const setData = ({name, login, followers, following, public_repos, avatar_url }) => {
+      setName(name)
+      setUsername(login)
+      setFollowers(followers)
+      setFollowing(following)
+      setRepos(public_repos)
+      setAvatar(avatar_url)
+    };
+
+    const handleSearch = (e) => {
+      setUserInput(e.target.value)
+    };
+
+    const handleSubmit = (e) => {
+      fetch(`https://api.github.com/users/${userInput}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.message) {
+          setError(data.message)
+        }
+        else{
+          setData(data);
+          setError(null);
+        };
+      })
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navbar">GitHub Search </div>
+    <div className="search">
+     <Form onSubmit={handleSubmit}>
+       <Form.Group>
+         <Form.Input placeholder="GitHub User" name="github user"
+         onChange={handleSearch}
+         />
+
+        <Form.Button className="search-button" context='Search' />
+       </Form.Group>
+       </Form> 
+       {error ? (<h1>{error}</h1>) : (<div className="card">
+       <Card>
+    <Image src={avatar} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{name}</Card.Header>
+      <Card.Header>{userName}</Card.Header>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        {follower} followers
+      </a>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        {repos} Repositories
+      </a>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        {following} Following
+      </a>
+    </Card.Content>
+  </Card>
+       </div>
+    )}
+    </div>   
     </div>
   );
 }
